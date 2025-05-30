ea062fca086f80846af8b5275142c88f38c8ff51
AngeloGianfelice <83078138+AngeloGianfelice@users.noreply.github.com>
2025-05-31 01:07:37 +0200

    removed pooling

--- source/models.py
+++ source/models.py
@@ -1,11 +1,15 @@
 import torch
-from torch_geometric.nn import global_add_pool, global_mean_pool, global_max_pool, GlobalAttention, Set2Set, SAGPooling, ASAPooling, EdgePooling, TopKPooling
+from torch_geometric.nn import global_add_pool, global_mean_pool, global_max_pool, GlobalAttention, Set2Set, EdgePooling, TopKPooling
 from source.conv import GNN_node, GNN_node_Virtualnode
 
 class GNN(torch.nn.Module):
-    def __init__(self, num_class=6, num_layer=5, emb_dim=300, 
-                 gnn_type='gin', virtual_node=True, residual=False, 
-                 drop_ratio=0.5, JK="last", graph_pooling="mean"):
+
+    def __init__(self, num_class, num_layer = 5, emb_dim = 300, 
+                    gnn_type = 'gin', virtual_node = True, residual = False, drop_ratio = 0.5, JK = "last", graph_pooling = "mean"):
+        '''
+            num_tasks (int): number of labels to be predicted
+            virtual_node (bool): whether to add virtual node or not
+        '''
 
         super(GNN, self).__init__()
 
@@ -17,14 +21,16 @@ class GNN(torch.nn.Module):
         self.graph_pooling = graph_pooling
 
         if self.num_layer < 2:
-            raise ValueError("Number of GNN layers must be > 1.")
+            raise ValueError("Number of GNN layers must be greater than 1.")
 
+        ### GNN to generate node embeddings
         if virtual_node:
-            self.gnn_node = GNN_node_Virtualnode(num_layer, emb_dim, JK=JK, drop_ratio=drop_ratio, residual=residual, gnn_type=gnn_type)
+            self.gnn_node = GNN_node_Virtualnode(num_layer, emb_dim, JK = JK, drop_ratio = drop_ratio, residual = residual, gnn_type = gnn_type)
         else:
-            self.gnn_node = GNN_node(num_layer, emb_dim, JK=JK, drop_ratio=drop_ratio, residual=residual, gnn_type=gnn_type)
+            self.gnn_node = GNN_node(num_layer, emb_dim, JK = JK, drop_ratio = drop_ratio, residual = residual, gnn_type = gnn_type)
 
-        # Setup pooling
+
+        ### Pooling function to generate whole-graph embeddings
         if self.graph_pooling == "sum":
             self.pool = global_add_pool
         elif self.graph_pooling == "mean":
@@ -32,19 +38,9 @@ class GNN(torch.nn.Module):
         elif self.graph_pooling == "max":
             self.pool = global_max_pool
         elif self.graph_pooling == "attention":
-            gate_nn = torch.nn.Sequential(
-                torch.nn.Linear(emb_dim, 2 * emb_dim),
-                torch.nn.BatchNorm1d(2 * emb_dim),
-                torch.nn.ReLU(),
-                torch.nn.Linear(2 * emb_dim, 1)
-            )
-            self.pool = GlobalAttention(gate_nn=gate_nn)
+            self.pool = GlobalAttention(gate_nn = torch.nn.Sequential(torch.nn.Linear(emb_dim, 2*emb_dim), torch.nn.BatchNorm1d(2*emb_dim), torch.nn.ReLU(), torch.nn.Linear(2*emb_dim, 1)))
         elif self.graph_pooling == "set2set":
-            self.pool = Set2Set(emb_dim, processing_steps=2)
-        elif self.graph_pooling == "sag":
-            self.pool = SAGPooling(emb_dim, ratio=0.5)
-        elif self.graph_pooling == "asap":
-            self.pool = ASAPooling(emb_dim)
+            self.pool = Set2Set(emb_dim, processing_steps = 2)
         elif self.graph_pooling == "edge":
             self.pool = EdgePooling(emb_dim)
         elif self.graph_pooling == "topk":
@@ -53,28 +49,15 @@ class GNN(torch.nn.Module):
             raise ValueError("Invalid graph pooling type.")
 
         if graph_pooling == "set2set":
-            self.graph_pred_linear = torch.nn.Linear(2 * self.emb_dim, self.num_class)
+            self.graph_pred_linear = torch.nn.Linear(2*self.emb_dim, self.num_class)
         else:
             self.graph_pred_linear = torch.nn.Linear(self.emb_dim, self.num_class)
+
 
     def forward(self, batched_data):
         h_node = self.gnn_node(batched_data)
 
-        if self.graph_pooling in ["sag", "asap"]:
-            # Adjust depending on your PyG version/pooling output
-            outputs = self.pool(h_node, batched_data.edge_index, batch=batched_data.batch)
-            # Try to unpack dynamically or check length:
-            if len(outputs) == 6:
-                h_node, edge_index, edge_attr, batch, perm, score = outputs
-            elif len(outputs) == 5:
-                h_node, edge_index, batch, perm, score = outputs
-                edge_attr = None
-            else:
-                raise ValueError(f"Unexpected output length {len(outputs)} from pooling")
-
-            h_graph = global_mean_pool(h_node, batch)
-
-        elif self.graph_pooling == "edge":
+        if self.graph_pooling == "edge":
             h_node, edge_index, batch, _ = self.pool(h_node, batched_data.edge_index, batched_data.batch)
             h_graph = global_mean_pool(h_node, batch)
 
